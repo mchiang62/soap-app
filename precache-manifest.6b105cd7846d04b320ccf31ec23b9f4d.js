a84f029d922f1a496182cf129da9c9346560626b
Melissa Chiang <mchiang62@gmail.com>
2020-01-14 20:07:51 -0500

    Updates

---
+++ precache-manifest.6b105cd7846d04b320ccf31ec23b9f4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0400506c66a587ec3b6b83dfcd46e726",
+    "url": "/soap-app/index.html"
+  },
+  {
+    "revision": "77eaf0e0af0798c2c07b",
+    "url": "/soap-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "613deb8083b00c250bfb",
+    "url": "/soap-app/static/js/2.cdc2379c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/soap-app/static/js/2.cdc2379c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77eaf0e0af0798c2c07b",
+    "url": "/soap-app/static/js/main.416ab4f9.chunk.js"
+  },
+  {
+    "revision": "110a2705b5f311226bdf",
+    "url": "/soap-app/static/js/runtime-main.75559643.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/soap-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
