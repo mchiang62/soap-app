06d0a655667edfa27efd03bdcfbb6fa5db9ab4b2
Dan Gallagher <dagallagher221@gmail.com>
2020-01-16 20:41:45 -0500

    added soaps controller

---
+++ controllers/soapsController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+//Defining methods for the soapsController
+
+module.exports = {
+    findAll: function(req, res) {
+        db.Soap
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        db.Soap
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.Soap
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.Soap
+            .findOneAndUpdate({_id: req.params.id}, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.Soap 
+            .findById({_id: req.params.id})
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
