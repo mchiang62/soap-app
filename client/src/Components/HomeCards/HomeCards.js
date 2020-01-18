e8e29092e19687e7897ea13c5500f4723da13c26
Melissa Chiang <mchiang62@gmail.com>
2020-01-18 12:03:20 -0500

    made changes to app.js and  header and added homecards

---
+++ client/src/Components/HomeCards/HomeCards.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./HomeCards.css";
+
+
+function HomeCards(props) {
+    return (
+      <div className="card">
+       <img src="https://www.thehistorymakers.org/sites/default/files/300x300" alt="placeholder" />
+        
+      </div>
+    );
+  }
+  
+  export default HomeCards;
+  
+
+  
+ 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import Header from "../Components/Header/Header";
+import HomeCards from "../Components/HomeCards/HomeCards";
+
+class App extends Component {
+    render() {
+      return (
+
+         <div className="App">
+          <Navbar />
+        
+             <Header><p>Welcome to Browning's Soap</p></Header>
+            
+            
+           <p className="App-intro">
+             Awesome Soap App
+           </p>
+
+        
+     <div className="container">
+          <div className="row">
+
+
+             <div className="col-4">
+             <HomeCards />
+            </div>
+             <div className="col-4">
+             <HomeCards />
+            </div>
+             <div className="col-4">
+             <HomeCards />
+            </div>
+
+
+        </div>
+     </div>
+
+           
+  
+  
+           
+         </div>
+        
+        
+        
+      );
+    }
+  }
+  
+  export default App;
+
+
+//   add jumbotron to header
+// add three cards - homecard component - need to insert to this page / col-4
