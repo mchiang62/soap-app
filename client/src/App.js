df1187f2c7e5cb154e08e1e134ea9b6f041677e5
Melissa Chiang <mchiang62@gmail.com>
2020-01-18 10:41:42 -0500

    minor edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Wrapper from './Components/Wrapper/Wrapper';
-import Card from './Components/Card/Card'
+import Card from './Components/Card/Card';
+
 
 class App extends Component {
   render() {
     return (
+      
 
       <div className="App">
+
+          
+
         <Wrapper>
           <Header><p>Welcome to Browning's Soap</p></Header>
           
