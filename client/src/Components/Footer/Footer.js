825a124dc6acbf904d78c0b8770acfb3b32f6bd4
edean78 <edean78@gmail.com>
2020-01-21 16:03:38 -0500

    created a simple footer

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -4,16 +4,16 @@ import "./Footer.css";
 function Footer() {
     return (
         <footer id="footer">
-            {/* <div className="row"> */}
-                {/* <div className="col-md-12"> */}
-                    <div className="card border-primary mb-12">
-                        <div className="card-body">
-                            <h4 className="card-title">Browning Soaps</h4>
-                            <p className="card-text">Thank you for visiting. Please check back weekly for new scents.</p>
-                        </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="title">
+                        <h5>Browning Soaps</h5>
+                        <p>Thanks for visiting, Please check back weekly for new scents</p>
+                        {/* <a href="https://www.facebook.com/browningssoap" title="Facebook Profile" target="_blank"><i class="fab fa-facebook" style="font-size: 15px; color: white;"></i></a>
+                        <a href="https://www.instagram.com/browningssoap/" title="Instagram Profile" target="_blank"><i class="fab fa-instagram" style="font-size: 15px; color: white;"></i></a> */}
                     </div>
-                {/* </div> */}
-            {/* </div> */}
+                </div>
+            </div>
         </footer>
     )
 }
