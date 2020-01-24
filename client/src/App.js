2601c6802ac280f2db5476f78dbfefcdff4788db
Dan Gallagher <dagallagher221@gmail.com>
2020-01-23 20:25:58 -0500

    fixed error in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react"
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Products from "./pages/Products";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
@@ -11,27 +11,30 @@ import Register from "./pages/Register";
 
 
 class App extends Component {
-      return (
+  render() {
+    return (
       <Router>
-       <div>
-         {/* <Navbar /> */}
+        <div className="App">
+          {/* <Navbar /> */}
           <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/cart" component={Cart} />
-          <Route exact path="/home" component={Home} />
-          <Route path="/about_us" component={About} />
-          <Route path="/contact_us" component={Contact} />
-          <Route path="/products" component={Products} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          {/* <Route component={NoMatch} /> */}
-        </Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cart" component={Cart} />
+            <Route exact path="/home" component={Home} />
+            <Route path="/about_us" component={About} />
+            <Route path="/contact_us" component={Contact} />
+            <Route path="/products" component={Products} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
 
-       </div>
-     </Router>
-  );
+        </div>
+      </Router>
+
+    )
+  };
 }
-  
+
 
 
 export default App;
