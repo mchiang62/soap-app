0ee69d81efb3518d3a3fe26e6cf2a4f5452a81fa
Dan Gallagher <dagallagher221@gmail.com>
2020-01-23 20:36:03 -0500

    fixed minor errors in App

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Products from "./pages/Products";
 import Home from "./pages/Home";
-import Products from "./pages/Products";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
