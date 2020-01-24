bb7f7c5c5d7349e88505254170199933cb420205
edean78 <edean78@gmail.com>
2020-01-24 16:42:07 -0500

    added get routes in the user.js file for login and register page

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const soapRoutes = require("./soaps");
 const userRoutes = require("./user");
 
-
 // DB routes
 router.use("/soaps", soapRoutes);
 router.use("/user", userRoutes);
