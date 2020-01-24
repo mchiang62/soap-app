a4c6ef46d920cc7af4f2beb2de641e7ed18eaa4f
edean78 <edean78@gmail.com>
2020-01-24 16:36:56 -0500

    created user.js in the routes directory and added a reference to this file in the index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const soapRoutes = require("./soaps");
+const userRoutes = require("./user");
 
 
 // DB routes
 router.use("/soaps", soapRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
