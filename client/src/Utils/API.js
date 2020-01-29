7ea69dec1efd51256d19b429956602e7b21f3df7
Dan Gallagher <dagallagher221@gmail.com>
2020-01-28 20:35:09 -0500

    test build

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -5,17 +5,17 @@ export default {
     getSoaps: function() {
         return axios.get("/api/soaps");
     },
-    getSoap: function(){
-        return axios.get("/api/soaps/" + id);
-    },
-    saveSoap: function() {
-        return axios.post("/api/soaps", soapData);
-    },
-    deleteSoap: function() {
-        return axios.delete("/api/soaps/" + id);
-    },
-    getPhoto: function() {
-        return axios.get("/api/photo" + id);
-    }
+    // getSoap: function(){
+    //     return axios.get("/api/soaps/" + id);
+    // },
+    // saveSoap: function() {
+    //     return axios.post("/api/soaps", soapData);
+    // },
+    // deleteSoap: function() {
+    //     return axios.delete("/api/soaps/" + id);
+    // },
+    // getPhoto: function() {
+    //     return axios.get("/api/photo" + id);
+    // }
     
 };
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
-import API from "../../../routes/api/API";
+import API from "../Utils/API";
 import {Input, FormBtn} from "../Components/AddSoapForm";
 
 
