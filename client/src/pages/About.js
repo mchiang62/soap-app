9c4315d89a2d7b9afdd22c7be3ddaf2b80b8ba4b
dashley06 <debashley06@gmail.com>
2020-01-30 21:15:13 -0500

    latest commit, products, and shopping cart

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import Navbar from "../Components/Navbar/Navbar"
 import Header from "../Components/Header/Header"
 import Footer from "../Components/Footer/Footer"
-import  "./about.css"
+//import  "./about.css"
 
 class About extends Component {
     render() {
