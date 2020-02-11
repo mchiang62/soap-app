180cef13d89d30f1f869132d61470ce89546e44c
dashley06 <debashley06@gmail.com>
2020-02-10 22:07:14 -0500

    google fonts, about me page, remove button

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-//import React, { Component } from "react"
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
@@ -7,7 +6,6 @@ import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ItemPage from './Shopping/ItemPage';
-import items  from './Shopping/static-data';
 import './App.css';
 import CartPage from './Shopping/CartPage';
 import Admin from "./pages/Admin";
@@ -18,7 +16,6 @@ const App = () => {
     <div className="App">
       <Router>
         <div className="App-Router fullWidth">
-          {/* <Navbar /> */}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/cart" component={CartPage} />
@@ -26,9 +23,7 @@ const App = () => {
              <Route exact path="/home" component={Home} />
              <Route path="/about_us" component={About} />
              <Route path="/contact_us" component={Contact} />
-             <Route path="/products" component={ItemPage} />
-             {/* <Route path="/products" render={(props) => (<ItemPage {...props} items={items} onAddToCart={addToCart} />)} /> */}
-             <Route path="/register" component={Register} />
+             <Route path="/products" component={ItemPage} />             <Route path="/register" component={Register} />
              <Route path="/login" component={Login} />
              <Route path="/admin" component={Admin}/>
              {/* <Route component={NoMatch} /> */}
--- client/src/Shopping/CartItem.js
+++ client/src/Shopping/CartItem.js
@@ -1,24 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import './ItemPage.css';
 import '../App.js';
 
 function CartItem(props){
-  const [cart, setCart] = useState([]);
-
-  const removeItem = props => {
-    let index = cart.findIndex(i => i.id === props.id);
-    if (index >= 0) {
-      // alert("Your Item has been deleted");
-      setCart(cart => {
-        const copy = [...cart];
-        copy.splice(index, 1);
-        return copy;
-      });
-    }
-  };
-  console.log("cartprops", {props})
-
-
+ //console.log({props}, "props")
   return(
     <div className="Item card">
         <div className="img-container">
@@ -32,10 +17,10 @@ function CartItem(props){
             <li>
               <strong>Price:</strong> {props.price}.00
             </li>
-            <button
+            {/* <button
               className="Item-remove"
-              onClick={() => removeItem(props.id)}>Remove Item
-            </button>
+              onClick={() => this.triggerDelete(props.id)}>Remove Item
+            </button> */}
           </ul>
         </div>
   </div>
